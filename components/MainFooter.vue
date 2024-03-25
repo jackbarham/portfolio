a6d93c45a1812d20366a3076517b06d852606f1f
Jack Barham <jack@airboxmedia.com>
2024-03-25 23:40:15 +0000

    Basic footer styling

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -1,6 +1,10 @@
 <template>
   <footer class="footer">
-    <p>Footer - &copy; {{ year }}</p>
+    <div class="bg-gray-950">
+      <div class="h-20 flex justify-between items-center max-w-7xl layout-w-normal">
+        <p class="text-white">Footer - &copy; {{ year }}</p>
+      </div>
+    </div>
   </footer>
 </template>
 
