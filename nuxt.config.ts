8e9489d17caa10eae3226272b8d72720b52e816d
Jack Barham <jack@airboxmedia.com>
2024-10-06 13:48:52 +0100

    Fonts V2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,38 +25,36 @@ export default defineNuxtConfig({
           type: 'image/png', 
           href: 'https://a.storyblok.com/f/299693/130x128/f7e6b5d5e7/favicon.png?v=1'
         },
-        // Preload the stylesheet first to avoid render-blocking but ensure it applies
-        { 
-          rel: 'preload', 
-          href: 'https://use.typekit.net/ztp5pvp.css',
-          as: 'style',
-        },
-        // Use the normal stylesheet without media switching to ensure the font loads
+        // { 
+        //   rel: 'preload', 
+        //   href: 'https://use.typekit.net/ztp5pvp.css',
+        //   as: 'style',
+        // },
         { 
           rel: 'stylesheet', 
           href: 'https://use.typekit.net/ztp5pvp.css',
         },
-        { 
-          rel: 'preconnect', 
-          href: 'https://use.typekit.net',
-        },
-        { 
-          rel: 'preconnect', 
-          href: 'https://p.typekit.net',
-          crossorigin: 'anonymous',
-        },
+        // { 
+        //   rel: 'preconnect', 
+        //   href: 'https://use.typekit.net',
+        // },
+        // { 
+        //   rel: 'preconnect', 
+        //   href: 'https://p.typekit.net',
+        //   crossorigin: 'anonymous',
+        // },
       ],
-      script: [
-        {
-          src: 'https://use.typekit.net/ztp5pvp.js',
-          async: true,
-          defer: true,
-        },
-        {
-          innerHTML: 'try{Typekit.load({ async: true });}catch(e){}',
-          type: 'text/javascript',
-        },
-      ]
+      // script: [
+      //   {
+      //     src: 'https://use.typekit.net/ztp5pvp.js',
+      //     async: true,
+      //     defer: true,
+      //   },
+      //   {
+      //     innerHTML: 'try{Typekit.load({ async: true });}catch(e){}',
+      //     type: 'text/javascript',
+      //   },
+      // ]
     }
   }
 })
