1c648e3c87c695063e9976af3682ec9d30103570
Jack Barham <jack@airboxmedia.com>
2024-10-06 20:31:32 +0100

    Responsive menu

--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -1,12 +1,11 @@
 <template>
   <header class="header">
-    <div class="h-20 flex justify-between items-center max-w-7xl layout-w-normal">
-      <h1 class="text-3xl">Jack Barham</h1>
-      <ul class="hidden md:flex space-x-16">
-        <li v-for="page in pages" :key="page.url">
+    <div class="md:h-20 md:flex justify-between items-center pt-8 max-w-7xl layout-w-normal">
+      <h1 class="text-3xl text-center md:text-left mb-4 md:mb-0">Jack Barham</h1>
+      <ul class="flex justify-center md:justify-normal md:space-x-16 lg:space-x-20">
+        <li v-for="page in pages" :key="page.url" class="px-6 md:px-0">
           <NuxtLink :to="page.url" class="relative group font-transducer font-medium tracking-wide">
-            <span class="block text-lg">{{ page.title }}</span>
-            <!-- Pseudo-element underline effect -->
+            <span class="block text-base md:text-lg">{{ page.title }}</span>
             <span class="relative block">
               <span class="absolute left-0 bottom-0 h-[2px] bg-black w-full transform scale-x-0 group-hover:scale-x-100 transition-transform duration-200 ease-in-out origin-left opacity-80"></span>
             </span>
