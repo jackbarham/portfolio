d5fc1cf09f15c0f6c908c64c8bfc635000d19b43
Jack Barham <jack@airboxmedia.com>
2024-10-06 12:06:42 +0100

    Switched to nuxt/fonts for Adobe Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,38 +31,7 @@ export default defineNuxtConfig({
           type: 'image/png', 
           href: 'https://a.storyblok.com/f/299693/130x128/f7e6b5d5e7/favicon.png?v=1'
         },
-        // { 
-        //   rel: 'preload', 
-        //   href: 'https://use.typekit.net/ztp5pvp.css',
-        //   as: 'style',
-        // },
-        // { 
-        //   rel: 'stylesheet', 
-        //   href: 'https://use.typekit.net/ztp5pvp.css',
-        //   onload: 'this.rel="stylesheet"',
-        //   media: 'print',
-        // },
-        // { 
-        //   rel: 'preconnect', 
-        //   href: 'https://use.typekit.net',
-        // },
-        // { 
-        //   rel: 'preconnect', 
-        //   href: 'https://p.typekit.net',
-        //   crossorigin: 'anonymous',
-        // },
       ],
-      // script: [
-      //   {
-      //     src: 'https://use.typekit.net/ztp5pvp.js',
-      //     async: true,
-      //     defer: true,
-      //   },
-      //   {
-      //     innerHTML: 'try{Typekit.load({ async: true });}catch(e){}',
-      //     type: 'text/javascript',
-      //   },
-      // ]
     }
   }
 })
