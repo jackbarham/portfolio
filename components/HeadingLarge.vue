582c8ca11c962b523f1800dc43db77e36f60f440
Jack Barham <>
2025-06-28 17:27:44 +0100

    Heading typography

--- components/HeadingLarge.vue
+++ components/HeadingLarge.vue
@@ -2,7 +2,7 @@
   <section class="heading-large">
     <div class="py-8 md:py-24">
       <h1 class="max-w-4xl font-heading font-black text-4xl md:text-5xl lg:text-6xl">{{ heading }}</h1>
-      <div v-if="intro" class="max-w-3xl text-base lg:text-xl mt-6" v-html="renderRichText(intro)"></div>
+      <div v-if="intro" class="max-w-3xl text-base font-medium lg:text-xl/8 mt-6" v-html="renderRichText(intro)"></div>
     </div>
   </section>
 </template>
