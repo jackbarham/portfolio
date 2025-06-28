e24c9a2a7bb2dca310c84f217c7ef00585bc75b0
Jack Barham <>
2025-06-28 11:26:49 +0100

    Allow for fixed header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col h-screen justify-between">
     <MainHeader />
-    <main role="main" class="mb-auto">
-      <div class="overflow-hidden py-16 md:py-24">
+    <main role="main" class="mb-auto pt-16 md:pt-20">
+      <div class="overflow-hidden">
         <slot />
       </div>
     </main>
