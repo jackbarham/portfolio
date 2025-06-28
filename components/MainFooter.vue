78ee52a86de3f280003cf933be3c1026d90d266d
Jack Barham <>
2025-06-28 17:35:06 +0100

    Footer text adjustment

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -4,18 +4,18 @@
       <div class="lg:flex justify-between items-center md:flex-row-reverse layout-full">
         <ul class="flex justify-center lg:justify-normal lg:mr-0 mb-12 lg:mb-0">
           <li v-for="social in socials" :key="social.id" class="px-1.5 md:p-0 md:ml-3">
-            <NuxtLink :to="social.url" target="_blank" class="block bg-white hover:bg-brand-red p-4 rounded-full" :aria-label="`${appTitle} on ${social.name}`">
+            <NuxtLink :to="social.url" target="_blank" class="block bg-gray-100 hover:bg-brand-red p-4 rounded-full" :aria-label="`${appTitle} on ${social.name}`">
               <span class="block h-9 w-9">
                 <component :is="icons[social.id]" filled class="text-black" />
               </span>
             </NuxtLink>
           </li>
         </ul>
-        <div class="text-gray-100 text-center lg:text-left">
-          <h1 class="text-3xl lg:text-2xl font-heading mb-1">
+        <div class="text-center lg:text-left">
+          <h1 class="text-3xl lg:text-2xl text-gray-100 font-heading mb-1">
             <NuxtLink to="/" class="hover:text-brand-red">{{ appTitle }}</NuxtLink>
           </h1>
-          <p class="text-sm lg:text-xs mb-12 lg:mb-6 max-w-50 lg:max-w-full mx-auto">Web development, interface design and digital consultancy.</p>
+          <p class="text-sm text-gray-200 mb-12 lg:mb-6 max-w-50 lg:max-w-full mx-auto">Web development, interface design and digital consultancy.</p>
           <p class="text-xs text-gray-400">Copyright &copy; 2010 - {{ year }} Airbox Media Ltd</p>
         </div>
       </div>
