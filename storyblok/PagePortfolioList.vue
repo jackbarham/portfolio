c00efa8af7a3b4205a39031367064395385de77b
Jack Barham <>
2025-06-30 10:35:59 +0100

    Renaming for CMS

---
+++ storyblok/PagePortfolioList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-editable="blok" class="page-portfolio">
+    <article class="layout-wide">
+      <div class="max-w-lg md:max-w-4xl lg:max-w-full mx-auto">
+        <HeadingLarge :heading="blok.heading" :intro="blok.intro" />
+        <div class="pb-8 md:pb-28">
+          <PortfolioGrid />
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+ 
+<script setup>
+defineProps({ blok: Object })
+</script>
