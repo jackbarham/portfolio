8bacfb6bf38d0366ea6c0c2bd20287dd503c395b
Jack Barham <>
2025-06-30 10:35:33 +0100

    Heading large responsive sizes

--- components/HeadingLarge.vue
+++ components/HeadingLarge.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="heading-large">
-    <div class="py-8 md:py-24">
+    <div class="py-10 md:py-16 lg:py-24">
       <h1 class="max-w-4xl font-heading font-black text-4xl md:text-5xl lg:text-6xl">{{ heading }}</h1>
-      <div v-if="intro" class="max-w-3xl text-base font-medium lg:text-xl/8 mt-6" v-html="renderRichText(intro)"></div>
+      <div v-if="intro" class="max-w-xl md:max-w-3xl text-base font-medium md:text-xl/7 mt-4 md:mt-6" v-html="renderRichText(intro)"></div>
     </div>
   </section>
 </template>
