71f5363486dc42b7155e873d43e6b0e1d1d9ea01
Jack Barham <>
2025-07-01 16:08:59 +0100

    Brands

---
+++ components/HeadingMini.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="mb-12 lg:mb-16 text-center">
+    <h1 class="text-3xl md:text-4xl font-[800]">{{ text }}</h1>
+  </header>
+</template>
+
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+})
+</script>
---
+++ storyblok/Brands.vue
@@ -0,0 +1,76 @@
+<template>
+  <section v-editable="blok" class="brands">
+    <div class="py-20 md:py-28 lg:py-40">
+      <HeadingMini :text="blok.heading" />
+      <div class="max-w-xs sm:max-w-2xl lg:max-w-6xl layout-full mx-auto">
+        <div class="grid grid-cols-3 lg:grid-cols-6 gap-10 md:gap-12 lg:gap-0 items-center justify-center">
+          <NuxtLink
+            v-for="brand in brands"
+            :key="brand.id"
+            :to="brand.url"
+            class="flex items-center justify-center lg:px-8 transition-opacity duration-200 hover:opacity-75"
+          >
+            <span :class="brandSizes[brand.id]" class="block opacity-95">
+              <component :is="icons[brand.id]" filled class="text-black w-full h-full" />
+            </span>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>  
+defineProps({ blok: Object })
+
+const brands = [
+  {
+    id: 'gsk',
+    name: 'GSK',
+    url: 'https://www.gsk.com',
+  },
+  {
+    id: 'netflix',
+    name: 'Netflix',
+    url: 'https://www.netflix.com',
+  },
+  {
+    id: 'virgin',
+    name: 'Virgin',
+    url: 'https://www.virgin.com',
+  },
+  {
+    id: 'mcdonalds',
+    name: 'McDonald\'s',
+    url: 'https://www.mcdonalds.com',
+  },
+  {
+    id: 'belmond',
+    name: 'Belmond',
+    url: 'https://www.belmond.com',
+  },
+  {
+    id: 'toyota',
+    name: 'Toyota',
+    url: 'https://www.toyota.com',
+  }
+]
+
+const icons = {
+  gsk: 'svgo-brand-gsk',
+  netflix: 'svgo-brand-netflix',
+  virgin: 'svgo-brand-virgin',
+  mcdonalds: 'svgo-brand-mcdonalds',
+  belmond: 'svgo-brand-belmond',
+  toyota: 'svgo-brand-toyota',
+}
+
+const brandSizes = {
+  gsk: 'w-24 h-18',
+  netflix: 'w-28 h-14',
+  virgin: 'w-40 h-16',
+  mcdonalds: 'w-18 h-18',
+  belmond: 'w-32 h-12',
+  toyota: 'w-32 h-16',
+}
+</script>
