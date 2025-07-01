f1601738fe0689833e73b43401c6053b3cdd14cd
Jack Barham <>
2025-07-01 19:57:29 +0100

    Testimonial template

---
+++ storyblok/testimonials.vue
@@ -0,0 +1,10 @@
+<template>
+  <section v-editable="blok" class="testimonials">
+    <div class="bg-brand-yellow py-20">
+    </div>
+  </section>
+</template>
+
+<script setup>
+defineProps({ blok: Object })
+</script>
