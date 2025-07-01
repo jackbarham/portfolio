04bee670194f68f5c65a08e11284658439c88371
Jack Barham <>
2025-07-01 07:25:21 +0100

    Mobile max width

--- components/PortfolioGrid.vue
+++ components/PortfolioGrid.vue
@@ -1,21 +1,23 @@
 <template>
   <section class="portfolio-grid">
-    <div class="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
-      <div v-for="(post, index) in posts" :key="post.uuid">
-        <NuxtLink :to="post.full_slug" class="block mb-4 group">
-          <div class="relative aspect-3/2 mb-2">
-            <NuxtImg
-              :loading="index === 0 ? 'eager' : 'lazy'"
-              :src="post.content.preview.filename"
-              :alt="post.content.heading"
-              sizes="360 sm:600 md:570"
-              format="webp"
-              class="w-full h-full object-cover rounded bg-gray-100 opacity-100 group-hover:opacity-80 transition-opacity duration-200"
-            />
-          </div>
-          <h2 class="text-2xl/9 md:text-xl/7 group-hover:text-brand-red duration-200">{{ post.content.heading }}</h2>
-          <span class="text-base text-gray-700">{{ post.content.type }}</span>
-        </NuxtLink>
+    <div class="max-w-lg md:max-w-full mx-auto">
+      <div class="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
+        <div v-for="(post, index) in posts" :key="post.uuid">
+          <NuxtLink :to="post.full_slug" class="block mb-4 group">
+            <div class="relative aspect-3/2 mb-2">
+              <NuxtImg
+                :loading="index === 0 ? 'eager' : 'lazy'"
+                :src="post.content.preview.filename"
+                :alt="post.content.heading"
+                sizes="360 sm:600 md:570"
+                format="webp"
+                class="w-full h-full object-cover rounded bg-gray-100 opacity-100 group-hover:opacity-80 transition-opacity duration-200"
+              />
+            </div>
+            <h2 class="text-2xl/9 md:text-xl/7 group-hover:text-brand-red duration-200">{{ post.content.heading }}</h2>
+            <span class="text-gray-600">{{ post.content.type }}</span>
+          </NuxtLink>
+        </div>
       </div>
     </div>
   </section>
