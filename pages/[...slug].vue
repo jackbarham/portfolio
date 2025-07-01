6f6f9992c9a0a6c12462960e068743f949eaa3fb
Jack Barham <>
2025-07-01 06:38:06 +0100

    Page transitions

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -10,3 +10,14 @@ const story = await useAsyncStoryblok(
   { version: 'draft' }
 )
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.2s ease;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
