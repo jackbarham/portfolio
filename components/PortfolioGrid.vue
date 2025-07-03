584a9648617b14cc89eb7104088a753eb864d8ea
Jack Barham <>
2025-07-03 20:08:28 +0100

    Arrow adjustment

--- components/PortfolioGrid.vue
+++ components/PortfolioGrid.vue
@@ -4,14 +4,14 @@
       <div class="grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-2 xl:grid-cols-3">
         <div v-for="(post, index) in posts" :key="post.uuid">
           <NuxtLink :to="post.full_slug" class="block group">
-            <div class="relative aspect-3/2 mb-2">
+            <div class="relative aspect-3/2 mb-2 bg-brand-yellow/40 rounded-md overflow-hidden">
               <NuxtImg
                 :loading="index === 0 ? 'eager' : 'lazy'"
                 :src="post.content.preview.filename"
                 :alt="post.content.heading"
-                sizes="360 sm:600 md:570"
+                sizes="400 sm:560 md:460 lg:590 xl:460"
                 format="webp"
-                class="w-full h-full object-cover rounded-md bg-gray-100 opacity-100 group-hover:opacity-80 transition-opacity duration-200"
+                class="w-full h-full object-cover opacity-100 group-hover:opacity-80 transition-opacity duration-200"
               />
             </div>
             <h2 class="text-2xl/9 md:text-xl/7 group-hover:text-brand-red duration-200">{{ post.content.heading }}</h2>
