11816c8280a0e8482ae6bc1508a0b8e06fb95eb8
Jack Barham <>
2025-07-07 11:29:57 +0100

    Navigation bar template

---
+++ components/NavigationBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="navigation-bar">
+    <div class="h-16 md:h-18 bg-brand-red">
+      <div class="layout-full">
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,9 @@
   <div class="flex flex-col h-screen justify-between">
     <MainHeader />
     <main role="main" class="mb-auto pt-16 md:pt-18">
-      <div class="overflow-hidden">
+      <div class="relative overflow-hidden">
         <slot />
+        <!-- <NavigationBar class="fixed bottom-0 w-full " /> -->
       </div>
     </main>
     <MainFooter />
