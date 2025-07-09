b127f35e083e2f8999687a8f68822189e9965cd0
Jack Barham <>
2025-07-09 19:11:51 +0100

    Heading and CTA adjustment

--- components/HeadingSection.vue
+++ components/HeadingSection.vue
@@ -1,10 +1,11 @@
 <template>
-  <header class="mb-10">
+  <header class="mb-8">
     <h1 class="text-4xl md:text-5xl font-black">{{ heading.heading }}</h1>
     <ButtonLight
       v-if="heading.cta_link.cached_url"
       :url="heading.cta_link.cached_url"
       :text="heading.cta_text"
+      class=" mt-3"
     />
   </header>
 </template>
