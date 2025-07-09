83ad6115a91f621959dea5b388b75a4df05118be
Jack Barham <>
2025-07-09 11:12:04 +0100

    Contact page structure

--- components/HeadingLarge.vue
+++ components/HeadingLarge.vue
@@ -2,7 +2,7 @@
   <header class="heading-large">
     <div class="py-10 md:py-16 lg:py-24">
       <h1 class="max-w-4xl font-heading font-black text-4xl md:text-5xl lg:text-6xl">{{ heading }}</h1>
-      <div v-if="intro" class="max-w-xl md:max-w-3xl text-base font-medium md:text-xl/7 lg:text-2xl/9 mt-4 md:mt-6" v-html="renderRichText(intro)"></div>
+      <div v-if="intro" class="max-w-xl md:max-w-3xl font-medium text-lg md:text-xl/7 lg:text-2xl/9 mt-4 md:mt-6" v-html="renderRichText(intro)"></div>
     </div>
   </header>
 </template>
---
+++ storyblok/PageContact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-editable="blok" class="page-about">
+    <article class="layout-wide">
+      <div class="py-10 md:py-16 lg:py-24">
+        <div class="prose prose-article">
+          <h1>{{ blok.heading }}</h1>
+          <article class="" v-html="renderRichText(blok.copy)"></article>
+          <a class="font-family-heading font-bold text-2xl" href="mailto:jack@airboxmedia.com">{{ blok.email }}</a>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+ 
+<script setup>
+defineProps({ blok: Object })
+</script>
