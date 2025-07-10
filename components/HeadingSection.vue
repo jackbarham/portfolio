cdbbcb4a00c913a7f22db5819467810c6f1d46d5
Jack Barham <>
2025-07-10 09:26:28 +0100

    Trailing slash on folder bug fix

--- components/HeadingSection.vue
+++ components/HeadingSection.vue
@@ -2,8 +2,8 @@
   <header class="mb-8">
     <h1 class="text-4xl md:text-5xl font-black">{{ heading.heading }}</h1>
     <ButtonLight
-      v-if="heading.cta_link.cached_url"
-      :url="heading.cta_link.cached_url"
+      v-if="heading.cta_link"
+      :url="heading.cta_link"
       :text="heading.cta_text"
       class=" mt-3"
     />
