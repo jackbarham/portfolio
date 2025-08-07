3967d70e9b6f5036827af06d94b98ffcdc3b2998
Jack Barham <>
2025-08-07 18:18:59 +0100

    New contact links and hide contact page

--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -15,7 +15,7 @@
         
         <!-- Desktop Navigation -->
         <nav aria-label="Main navigation">
-          <ul class="hidden md:flex md:space-x-12 lg:space-x-20 mb-0.5">
+          <ul class="hidden md:flex md:space-x-12 lg:space-x-20 mb-0.5 items-center">
             <li v-for="page in pages" :key="page.url">
               <NuxtLink :to="page.url" :aria-label="page.title" class="relative group">
                 <span class="block font-heading font-medium tracking-wide text-base text-white md:text-lg">{{ page.title }}</span>
@@ -23,6 +23,14 @@
                   <span class="absolute left-0 bottom-0 h-[2px] bg-white w-full transform scale-x-0 group-hover:scale-x-100 transition-transform duration-200 ease-in-out origin-left opacity-90"></span>
                 </span>
               </NuxtLink>
+            </li>
+            <li>
+              <a href="mailto:jack@airboxmedia.com" class="inline-flex items-center bg-white h-10 px-4 rounded">
+                <span class="inline-block h-6 w-6 mr-2">
+                  <svgo-email filled class="text-brand-purple" />
+                </span>
+                <span class="block font-heading font-medium tracking-wide text-base text-brand-purple md:text-lg">Contact</span>
+              </a>
             </li>
           </ul>
         </nav>
@@ -60,6 +68,21 @@
           >{{ page.title }}</NuxtLink>
         </li>
       </ul>
+
+      <!-- CTA button -->
+      <div 
+        class="absolute bottom-12 min-w-48 transform transition-opacity duration-[700ms] ease-in-out"
+        :class="menuOpen ? 'opacity-100' : 'opacity-0'"
+        :style="{ 
+          transitionDelay: menuOpen ? '200ms' : '0ms',
+          transitionDuration: menuOpen ? '700ms' : '150ms'
+        }"
+      >
+        <a href="mailto:jack@airboxmedia.com" class="">
+          <span class="inline-block text-white/70 mb-2">Send a message</span>
+          <span class="block text-xl font-heading font-medium tracking-wide text-white md:text-lg">jack@airboxmedia.com</span>
+        </a>
+      </div>
       
     </nav>
 
@@ -82,10 +105,10 @@ const pages = [
     title: 'Portfolio',
     url: '/portfolio'
   },
-  {
-    title: 'Contact',
-    url: '/contact'
-  }
+  // {
+  //   title: 'Contact',
+  //   url: '/contact'
+  // }
 ]
 
 // Mobile menu state
