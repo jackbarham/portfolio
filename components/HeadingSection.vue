1ca9112f3b289c8b12c56ae8de4cb066cf76bd72
Jack Barham <>
2025-08-07 17:59:54 +0100

    CTA button

--- components/HeadingSection.vue
+++ components/HeadingSection.vue
@@ -1,11 +1,11 @@
 <template>
-  <header class="mb-8">
+  <header class="flex items-center justify-between mb-12">
     <h1 class="text-4xl md:text-5xl font-black">{{ heading.heading }}</h1>
     <ButtonLight
       v-if="heading.cta_link"
       :url="heading.cta_link"
       :text="heading.cta_text"
-      class=" mt-3"
+      class="hidden lg:block"
     />
   </header>
 </template>
--- storyblok/PortfolioFeatured.vue
+++ storyblok/PortfolioFeatured.vue
@@ -3,9 +3,26 @@
     <div class="bg-brand-grey-light py-16 md:py-20 lg:py-28">
       <div class="layout-wide">
         <div class="max-w-lg md:max-w-full mx-auto">
-          <HeadingSection :heading="blok" />
+          <!-- <HeadingSection :heading="blok" /> -->
+          <header class="md:flex items-center justify-between md-8 md:mb-12">
+            <h1 class="text-4xl md:text-5xl font-black">{{ blok.heading }}</h1>
+            <ButtonLight
+              v-if="blok.cta_link"
+              :url="blok.cta_link"
+              :text="blok.cta_text"
+              class="invisible md:visible"
+            />
+          </header>
+          <PortfolioGrid :posts="resolvedData" />
+          <ButtonLight
+            v-if="blok.cta_link"
+            :url="blok.cta_link"
+            :text="blok.cta_text"
+            class="visible md:invisible mt-8"
+          />
         </div>
-        <PortfolioGrid :posts="resolvedData" />
+        
+        
       </div>
     </div>
   </section>
