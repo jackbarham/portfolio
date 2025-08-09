767167cd4e63421957f07182535a8a6fcd5d9ad1
Jack Barham <>
2025-08-09 22:22:40 +0100

    Revert back to light grey

--- components/ContactBlock.vue
+++ components/ContactBlock.vue
@@ -3,7 +3,7 @@
     <div class="bg-brand-grey-light py-20 md:py-24">
       <div class="layout-wide">
         <div class="max-w-2xl mx-auto px-8 md:px-0">
-          <h1 class="text-4xl mb-7">{{ content.contact_heading }}</h1>
+          <h1 class="text-3xl mb-7">{{ content.contact_heading }}</h1>
           <div class="prose prose-article">
             <article class="!font-medium mb-8" v-html="renderRichText(content.contact_copy)"></article>
             <a class="inline-flex font-semibold items-center text-xl" :href="`mailto:${content.email}`">{{ content.email }}</a>
